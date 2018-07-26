1ec4edecdb1d8f79a4d66bfaabb0ca162ddb7399
Carlos <cs4026@stern.nyu.edu>
2018-07-26 05:15:50 -0400

    added redbired

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 const express = require('express');
 const path = require('path');
+let path = require('path');
+var proxy = require('redbird')({port: 80});
 const app = express();
 
+proxy.register("nickshaya.com", "18.222.20.107:8080");
+proxy.register("www.nickshaya.com","18.222.20.107:8080")
 
 app.use(express.static(path.join(__dirname, '/build')));
 app.use(compression());
@@ -15,7 +19,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
-const port = 3000;
+const port = 8080;
 app.listen(port);
 
 console.log(`Landing listening on ${port}`);
